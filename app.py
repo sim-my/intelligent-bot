7c29dbaeccdcb6fbd14b7cda756456e179b315b0
sim-my <simranpanthi101@gmail.com>
2020-10-06 15:41:42 +0545

    Created the design of the application

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/ask')
+def ask():
+    return render_template('ask.html')
