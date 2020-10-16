206250b68ecf98caed1b384de3f5a7e69aa8f4ec
sim-my <simranpanthi101@gmail.com>
2020-10-16 17:38:01 +0545

    Completed intelligent bot

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, session
+from model import tokenize, bot_response
+from flask_session import Session
 app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/ask', methods=['POST'])
-def ask():
+@app.route('/send', methods=['POST'])
+def sendData():
     if(request.form.get('input-url')):
         value = request.form.get('input-url')
+        session['sentence_list'] = tokenize('url',value)
     elif(request.form.get('input-text')):
         value = request.form.get('input-text')
-    return render_template('ask.html', value = value)
+        session['sentence_list'] = tokenize('text',value)
+    return render_template('ask.html', sentence_list = session['sentence_list'])
+
+@app.route("/ask")
+def get_bot_response():
+    if(session.get('sentence_list')):
+        userText = request.args.get('msg') 
+        list_of_sent = session['sentence_list'] 
+        answer = bot_response(userText, list_of_sent )
+        return answer
+    else:
+        return 'No answer'
+
+    
--- model.py
+++ model.py
@@ -11,12 +11,15 @@ warnings.filterwarnings('ignore')
 
 nltk.download('punkt')
 
-def tokenize(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    article.nlp()
-    corpus = article.text   
+def tokenize(value_type, value):
+    if(value_type == 'url'):
+        article = Article(value)
+        article.download()
+        article.parse()
+        article.nlp()
+        corpus = article.text   
+    elif(value_type == 'text'):
+        corpus=value
     sentence_list = nltk.sent_tokenize(corpus)
     return sentence_list
 
@@ -26,12 +29,12 @@ def bot_response(user_input, sentence_list):
     bot_response = ' '
     cm = CountVectorizer().fit_transform(sentence_list)
     similarity_score = cosine_similarity(cm[-1], cm)
+    sentence_list.pop()
     similarity_score_list = similarity_score.flatten()   
     sorted_similarity_score_list = sorted(similarity_score_list, reverse=True)    
     sorted_similarity_score_list = sorted_similarity_score_list[1:]
     answer_index = np.where(similarity_score_list==sorted_similarity_score_list[0])[0][0]
     answer = sentence_list[answer_index]
-    print(answer)
+    return answer
 
-sentence_list = tokenize('https://en.wikipedia.org/wiki/Machine_learning')
-bot_response('What is machine learning?',sentence_list)
+sentence_list = tokenize('url','https://www.internetsociety.org/resources/doc/2017/artificial-intelligence-and-machine-learning-policy-paper/?gclid=CjwKCAjwiaX8BRBZEiwAQQxGx0qpU6qYi2LRF8bPdQZFxTR2e1EXuLQGMBML5m6NUk7WqOefCuRGfRoCDegQAvD_BwE')
